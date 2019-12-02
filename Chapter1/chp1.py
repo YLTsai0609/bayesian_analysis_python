1f4badcb8a17ca33a16ce28535a262e0cda688d5
YLTsai0609 <yltsai0609@gmail.com>
2019-12-02 22:52:20 +0800

    [Content] 9/10 Bayesian inference chap1

--- Chapter1/chp1.py
+++ Chapter1/chp1.py
@@ -10,7 +10,7 @@ plt.style.use('fivethirtyeight')
 
 # %config InlineBackend.figure_format = 'retina'
 
-FILE_PATH = '../images/Binnomial.png'
+FILE_PATH_bi = '../images/Binnomial.png'
 
 n_params = [1, 2, 4, 8]
 p_params = [0.25, 0.5, 0.75]
@@ -31,6 +31,106 @@ ax[1,0].set_ylabel('$p(y|\\theta)$', fontsize=14)
 ax[0,0].set_xticks(x)
 save = input("Save the figure? y/n")
 if save == y:
-    plt.savefig(FILE_PATH, dpi=300, figsize=(12, 8))
+    plt.savefig(FILE_PATH_bi, dpi=300, figsize=(12, 8))
+
+FILE_PATH_beta = '../images/Binnomial.png'
+
+params = [0.5, 1, 2, 3]
+x = np.linspace(0, 1, 100)
+f, ax = plt.subplots(len(params), len(params), sharex=True, 
+  sharey=True, figsize=(12, 8))
+for i in range(4):
+    for j in range(4):
+        a = params[i]
+        b = params[j]
+        y = stats.beta(a, b).pdf(x) # the meats
+        ax[i,j].plot(x, y)
+        ax[i,j].plot(0, 0, label="$\\alpha$ = {:3.2f}\n$\\beta$ = {:3.2f}".format(a, b), alpha=0)
+        ax[i,j].legend(fontsize=12)
+ax[3,0].set_xlabel('$\\theta$', fontsize=14)
+ax[0,0].set_ylabel('$p(\\theta)$', fontsize=14)
+save = input("Save the figure? y/n")
+if save == y:
+    plt.savefig(FILE_PATH_beta, dpi=300, figsize=(12, 8))
+
+FILE_PATH_Posterior = '../images/Posterior.png'
+
+plt.subplot?
+
+# +
+# A better plot?
+theta_real = 0.35
+trials = [0, 1, 2, 3, 4, 8, 16, 32, 50, 150]
+data = [0, 1, 1, 1, 1, 4, 6, 9, 13, 48]
+
+beta_params = [(1, 1), (0.5, 0.5), (20, 20)]
+dist = stats.beta
+x = np.linspace(0, 1, 100)
+
+for idx, N in enumerate(trials):
+    if idx == 0:
+        plt.subplot(4,3, 2)
+    else:
+        plt.subplot(4,3, idx+3)
+    y = data[idx]
+    for (a_prior, b_prior), c in zip(beta_params, ('b', 'r', 'g')):
+        p_theta_given_y = dist.pdf(x, a_prior + y, b_prior + N - y)
+        plt.plot(x, p_theta_given_y, c)
+        plt.fill_between(x, 0, p_theta_given_y, color=c, alpha=0.6)
+
+    plt.axvline(theta_real, ymax=0.3, color='k')
+    plt.plot(0, 0, label="{:d} experiments\n{:d} heads".format(N, y), alpha=0)
+    plt.xlim(0,1)
+    plt.ylim(0,12)
+    plt.xlabel(r"$\theta$") 
+    plt.legend()
+    plt.gca().axes.get_yaxis().set_visible(False)
+plt.tight_layout()
+
+save = input("Save the figure? y/n")
+if save == y:
+    plt.savefig(FILE_PATH_Posterior, dpi=300, figsizes=(12, 8))
+
+# +
+NAIVE_HPD = '../images/Naive_hpd.png'
+def naive_hpd(post):
+    sns.kdeplot(post)
+    HPD = np.percentile(post, [2.5, 97.5])
+    plt.plot(HPD, [0, 0], label='HPD {:.2f} {:.2f}'.format(*HPD), 
+      linewidth=8, color='k')
+    plt.legend(fontsize=16);
+    plt.xlabel(r"$\theta$", fontsize=14)
+    plt.gca().axes.get_yaxis().set_ticks([])
+
+    
+np.random.seed(1)
+post = stats.beta.rvs(5, 11, size=1000)
+naive_hpd(post)
+plt.xlim(0, 1)
+
+save = input("Save the figure? y/n")
+if save == y:
+    plt.savefig(NAIVE_HPD, dpi=300, figsize=(5.5, 5.5))
+
+# +
+np.random.seed(1)    
+gauss_a = stats.norm.rvs(loc=4, scale=0.9, size=3000)
+gauss_b = stats.norm.rvs(loc=-2, scale=1, size=2000)
+mix_norm = np.concatenate((gauss_a, gauss_b))
+
+naive_hpd(mix_norm)
+print('Wrong HPD =( ')
+# -
+
+# Multiple peak
+MULTIPLE_PEAK_HPD = '../images/Multiple_peak_hpd.png'
+from plot_post import plot_post
+plot_post(mix_norm, roundto=2, alpha=0.05)
+plt.legend(loc=0, fontsize=10)
+plt.xlabel(r"$\theta$", fontsize=14)
+save = input("Save the figure? y/n")
+if save == y:
+    plt.savefig(MULTIPLE_PEAK_HPD, dpi=300, figsize=(12, 8))
+
 
 
